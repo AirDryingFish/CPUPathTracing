a263a6ac25fd136a07d20cccd403a2d6e59cf012
Yang Zengzhi[SDX2525] <zengzhi.yang@garena.com>
2025-11-21 13:21:08 +0800

    Add something

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "color.h"
 int main(){
     int image_width = 256;
     int image_height = 256;
@@ -7,14 +8,9 @@ int main(){
     for (int j = 0; j < image_height; j++){
         std::clog << "\rScanline remaining: " << image_height - j << ' ' << std::flush; 
         for (int i = 0; i < image_width; i++){
-            double r = double(j) / (image_height - 1);
-            double g = double(i) / (image_width - 1);
-            double b = 0.0; // constant blue value
-
-            int ir = int (255.999 * r);
-            int ig = int (255.999 * g);
-            int ib = int (255.999 * b);
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            write_color(std::cout, color(double(i) / (image_width - 1),
+                                         double(j) / (image_height - 1),
+                                         0));
         }
     }
     std::clog << "\rDone.               \n";
---
+++ vec3.h
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <cmath>
+#ifndef VEC3_H
+#define VEC3_H
+
+// Basic 3D vector class
+class vec3 {
+public:
+    double e[3];
+    vec3(): e{0, 0, 0} {}
+    vec3(double e0, double e1, double e2): e{e0, e1, e2} {}
+    double x() const { return e[0]; }
+    double y() const { return e[1]; }
+    double z() const { return e[2]; }
+
+    vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+    double operator[](int i) const {return e[i];}
+    double& operator[](int i) {return e[i];}
+
+    vec3& operator+=(const vec3 & other){
+        e[0] += other.e[0];
+        e[1] += other.e[1];
+        e[2] += other.e[2];
+        return *this;
+    }
+
+    vec3& operator*=(const double t){
+        e[0] *= t;
+        e[1] *= t;
+        e[2] *= t;
+        return *this;
+    }
+
+    vec3& operator/=(const double t){
+        *this *= 1 / t;
+        return *this;
+    }
+
+    double length_squared() const {
+        return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+    }
+
+    double length() const {
+        return std::sqrt(length_squared());
+    }
+};
+
+// Make an alias for vec3 as point3
+using point3 = vec3;
+
+inline std::ostream& operator<<(std::ostream& out, const vec3& v){
+    return out << "[vec3 Info] " << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+}
+
+inline vec3 operator+(const vec3& u, const vec3& v){
+    return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
+}
+
+inline vec3 operator-(const vec3& u, const vec3& v){
+    return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
+}
+
+inline vec3 operator*(const vec3& u, const vec3& v){
+    return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+}
+inline vec3 operator*(const vec3& u, double t){
+    return vec3(u.e[0] * t, u.e[1] * t, u.e[2] * t);
+}
+inline vec3 operator*(double t, const vec3& u){
+    return u * t;
+}
+
+inline vec3 operator/(const vec3& u, double t){
+    return u * (1 / t);
+}
+
+inline double dot(const vec3& u, const vec3& v){
+    return u.e[0] * v.e[0]
+         + u.e[1] * v.e[1]
+         + u.e[2] * v.e[2];
+}
+
+inline vec3 cross(const vec3& u, const vec3& v){
+    return vec3(
+        u.e[1] * v.e[2] - u.e[2] * v.e[1],
+        u.e[2] * v.e[0] - u.e[0] * v.e[2],
+        u.e[0] * v.e[1] - u.e[1] * v.e[0]
+    );
+}
+
+inline vec3 normalize(const vec3& v) {
+    return v / v.length();
+}
+
+
+#endif
